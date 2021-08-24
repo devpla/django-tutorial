ece0ca2e47ecd7d873878a21f3b50ba1f0819a50
hing9u <yeonheechae425@gmail.com>
2021-08-24 22:03:57 +0900

    feat: Write views
    resolved #10
    
    - 뷰 및 뷰 기능 추가
    - Http404 에러 코드 작성
    - 템플릿 생성 및 render()로 연결
    - urls.py에 이름공간 정의

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    # HttpResponse 객체와 자주 쓰는 표현 단축 : render
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
